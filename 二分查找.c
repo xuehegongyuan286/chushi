a6bd69a9401f9d916a34c119e717174e7c3e5720
xuehegongyuan286 <147366524+xuehegongyuan286@users.noreply.github.com>
2023-11-29 18:25:52 +0800

    Add files via upload

---
+++ 二分查找.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+ {
+	int n = 0;
+	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int le = 0;
+	int rig = sz-1;
+	scanf_s("%d",&n);
+	while (le <= rig)
+	{
+		int mid = (rig - le) / 2+le;
+		if (arr[mid] < n)
+		{
+			le = mid + 1;
+	    }
+		else if (arr[mid] > n)
+		{
+			rig = mid - 1;
+		}
+		else 
+		{
+			printf("该数字所对应的数组下标是：%d", mid);
+			break;
+		}
+	}
+	if (le > rig)
+	{
+		printf("输入错误\n");
+	}
+}
